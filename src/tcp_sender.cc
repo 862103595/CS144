ff400bdfc80384d0762d4af082483ec9411ed096
JHXie <862103595@qq.com>
2024-09-13 22:30:02 +0800

    finish lab3

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -6,37 +6,88 @@ using namespace std;
 uint64_t TCPSender::sequence_numbers_in_flight() const
 {
   // Your code here.
-  return {};
+  return total_outstanding;
 }
 
 uint64_t TCPSender::consecutive_retransmissions() const
 {
   // Your code here.
-  return {};
+  return retran_uint64_;
 }
 
 void TCPSender::push( const TransmitFunction& transmit )
 {
   // Your code here.
-  (void)transmit;
+  if(has_fin) return;
+  uint64_t pretend = (window == 0) ? 1 : window;
+  uint64_t left_window = pretend - total_outstanding;
+  if(window < total_outstanding) left_window = 0;
+  while(left_window > 0 && !has_fin) {
+    TCPSenderMessage new_message = make_empty_message();
+    if(!has_sent) { new_message.SYN = true;  has_sent = true;}
+    string payload;
+    read( input_.reader(), min(left_window-new_message.sequence_length(), TCPConfig::MAX_PAYLOAD_SIZE), payload );
+    new_message.payload = std::move( payload );
+    if(input_.reader().is_finished() && left_window > new_message.sequence_length()) { new_message.FIN = true; has_fin = true;}
+
+    if(new_message.sequence_length() == 0) return;
+    total_outstanding += new_message.sequence_length();
+    next_abs += new_message.sequence_length();
+    outstanding.push( new_message );
+    transmit(new_message);
+    left_window -= new_message.sequence_length();
+    timer.set_opened();
+  }
+
 }
 
 TCPSenderMessage TCPSender::make_empty_message() const
 {
   // Your code here.
-  return {};
+  return {isn_ + next_abs, false, std::string(), false, input_.has_error()};
 }
 
 void TCPSender::receive( const TCPReceiverMessage& msg )
 {
   // Your code here.
-  (void)msg;
+  window = msg.window_size;
+  if(msg.RST) {input_.set_error();}
+  if(!msg.ackno.has_value()) { return; }
+  uint64_t new_ackno = msg.ackno->unwrap( isn_, input_.reader().bytes_popped() );
+  if(new_ackno > next_abs) return;
+
+
+  if(new_ackno <= ackno) return;
+  ackno = new_ackno;
+
+  if(outstanding.empty()) return;
+  TCPSenderMessage front = outstanding.front();
+  while(front.seqno.unwrap( isn_, ackno ) + front.sequence_length() <= ackno) {
+    total_outstanding -= front.sequence_length();
+    outstanding.pop();
+    if(outstanding.empty()) break;
+    front = outstanding.front();
+  }
+  timer.reset();
+  if(outstanding.empty()) {timer.set_closed();}
+  else { timer.set_zero();}
+  retran_uint64_ = 0;
 }
 
 void TCPSender::tick( uint64_t ms_since_last_tick, const TransmitFunction& transmit )
 {
   // Your code here.
-  (void)ms_since_last_tick;
-  (void)transmit;
-  (void)initial_RTO_ms_;
+  if(timer.is_closed()) {
+    return;
+  }
+  if(timer.tick( ms_since_last_tick )) {
+    transmit(outstanding.front());
+
+    if(window != 0) {
+      retran_uint64_ +=1;
+      timer.double_RTO();
+    }
+
+    timer.set_zero();
+  }
 }
--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -10,13 +10,54 @@
 #include <memory>
 #include <optional>
 #include <queue>
+#include <map>
+
+class RetransmissionTimer
+{
+public:
+  explicit RetransmissionTimer(uint64_t initial_RTO_ms):initial_RTO(initial_RTO_ms) ,RTO(initial_RTO_ms) {};
+
+  void set_closed() {closed_ = true;}
+
+  void set_opened() {closed_ = false;}
+
+  bool is_closed() const {return closed_;}
+
+  bool tick(uint64_t ms_since_last_tick)
+  {
+    retransmission_time += ms_since_last_tick;
+    if (retransmission_time >= RTO) return true;
+    return  false;
+  }
+
+  void double_RTO()
+  {
+    RTO *= 2;
+  }
+
+  void set_zero()
+  {
+    retransmission_time = 0;
+  }
+
+  void reset()
+  {
+    RTO = initial_RTO;
+  }
+
+private:
+  const uint64_t initial_RTO;
+  uint64_t RTO;
+  uint64_t retransmission_time{ };
+  bool closed_ {  true };
+};
 
 class TCPSender
 {
 public:
   /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
   TCPSender( ByteStream&& input, Wrap32 isn, uint64_t initial_RTO_ms )
-    : input_( std::move( input ) ), isn_( isn ), initial_RTO_ms_( initial_RTO_ms )
+    : input_( std::move( input ) ), isn_( isn ), initial_RTO_ms_( initial_RTO_ms ), timer(initial_RTO_ms)
   {}
 
   /* Generate an empty TCPSenderMessage */
@@ -35,7 +76,7 @@ public:
   void tick( uint64_t ms_since_last_tick, const TransmitFunction& transmit );
 
   // Accessors
-  uint64_t sequence_numbers_in_flight() const;  // How many sequence numbers are outstanding?
+  uint64_t sequence_numbers_in_flight() const;  // How many sequence numbers are outstanding/?
   uint64_t consecutive_retransmissions() const; // How many consecutive *re*transmissions have happened?
   Writer& writer() { return input_.writer(); }
   const Writer& writer() const { return input_.writer(); }
@@ -48,4 +89,13 @@ private:
   ByteStream input_;
   Wrap32 isn_;
   uint64_t initial_RTO_ms_;
+  std::queue<TCPSenderMessage> outstanding {};
+  uint64_t retran_uint64_ {};
+  uint64_t total_outstanding {};
+  uint64_t window {1};
+  u_int64_t ackno {};
+  u_int64_t next_abs { };
+  RetransmissionTimer timer;
+  bool has_sent { };
+  bool has_fin { };
 };
